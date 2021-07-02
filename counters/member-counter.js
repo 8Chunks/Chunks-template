364c6cb17459203e7ab9a8ed3827bdeaf861d286
Evan Axon <72518692+8Chunks@users.noreply.github.com>
2021-07-02 16:27:08 +0100

    Release

--- counters/member-counter.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (client) => {
-    const guild = client.guilds.cache.get('789863108176642088'); // Put your guild ID
-    setInterval(() => {
-        const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('858082853286707231'); // Arism put your voice channel id 
-        channel.setName(`Jayparm's Members - ${memberCount.toString()}`)
-        console.log('Updating Member Count');
-    },300000);
-}
--- events/ready.js
+++
@@ -1,8 +0,0 @@
-client.on('ready', () => {
-    console.log(`Logged in as user: ${client.user.tag}, ID: ${client.user.id}`);
-    client.user.setStatus('online');
-    client.user.setActivity("Fortnite", {
-        type: "PLAYING",
-    });
-   // memberCounter(client)
-});
